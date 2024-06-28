b3bcac456d739dd6d0f01cb342dfcee1937072ba
Станис <102852335+ImitoGSY@users.noreply.github.com>
2024-06-28 23:24:00 +1000

    DZ3
    условие if и цикл for. Во время проверки понизил скорость игры. В итоге вернул обратно (_timeScale)

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -16,13 +16,21 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+            float t = GetTemperature();
+            IncreaseTemperature();
+            if (t >= overheatTemperature)
+            {
+                return;
+            }
+
+            for (float i = GetTemperature(); i <= 3 && i>0 ; i--)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
         }
+
+
 
         public override Vector2Int GetNextStep()
         {
